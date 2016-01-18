2ab30bc0f3a3f5e9de12ee4999dbe1915b6b3952
Whapow <joshuavancamp@gmail.com>
2016-01-18 16:43:48 -0500

    fixing markdown on README

--- sign-defaults.ps1
+++ sign-defaults.ps1
@@ -1,6 +1,4 @@
 ﻿$defaultScripts = '.\csv_download_and_process.ps1', '.\config\config.ps1', '.\functions.ps1', '.\process_hook.ps1', '.\failure_hook.ps1', '.\finish_hook.ps1'
-$autoSign = '.\sign-script.ps1'
-
 forEach ($script in $defaultScripts){
-    &$autoSign $script
+    Set-AuthenticodeSignature $script @(Get-ChildItem cert:\CurrentUser\My -codesign)[0]
 }
